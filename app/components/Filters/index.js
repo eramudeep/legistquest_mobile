489a9d51f6e4cc7a5e1f7ea8f1d557eb92e3281b
noddy1996 <shammi.rathore66@gmail.com>
2021-08-04 10:35:03 +0530

    filter issue resolve

--- app/components/Filters/index.js
+++ app/components/Filters/index.js
@@ -5,9 +5,11 @@ import {connect} from 'react-redux';
 import { toggleFilters } from '../../redux/filterActions';
  
  function index({Court,filters,toggleFilters$}) {
+   
   return (
     <ScrollView>
       {Object.keys(Court)?.map((item, key) => {
+        if(Court[item].length>0)
         return <FilterComp key={key} filters={filters} toggleFilters$={toggleFilters$} label={item} Court={Court[item]} />;
       })}
     </ScrollView>
