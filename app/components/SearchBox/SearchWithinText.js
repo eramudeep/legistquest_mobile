2581af527836382ffaf71f3a0f7ca745f9a89519
Aman Deep <mndeep204@gmail.com>
2021-08-09 15:44:53 +0530

    searchwithin

--- app/components/SearchBox/SearchWithinText.js
+++ app/components/SearchBox/SearchWithinText.js
@@ -1,26 +1,23 @@
 import React, {useState} from 'react';
-import {View, Text, FlatList, Pressable,StyleSheet} from 'react-native';
+import {View, Text, FlatList, Pressable, StyleSheet} from 'react-native';
 import {scale} from 'react-native-size-matters';
 import CustomIcon from '../CustomIcon/Icon';
 import CustomInput from '../CustomInput';
 import CustomLabel from '../CustomLabel/CustomLabel';
 import {connect} from 'react-redux';
-import { SEARCH_WITH_IN_CASE } from '../../services/ApiList';
+import {SEARCH_WITH_IN_CASE} from '../../services/ApiList';
 import HTML from 'react-native-render-html';
 import Highlighter from 'react-native-highlight-words';
-import { appColors } from '../../utils/appColors';
-import { removeHtmlTags, truncateString } from '../../utils/common';
-function SearchWithinText({
-  navigation,
-  route,
-}) {
+import {appColors} from '../../utils/appColors';
+import {removeHtmlTags, truncateString} from '../../utils/common';
+function SearchWithinText({navigation, route}) {
   console.log(route.params);
-  const{id}=route.params
+  const {id} = route.params;
   const [isFocsed, setIsFocsed] = useState(false);
-//   const [isFocsedSection, setIsFocsedSection] = useState(false);
+  //   const [isFocsedSection, setIsFocsedSection] = useState(false);
   const [searchedQuery, setSearchedQuery] = useState();
-  const [searchwithinResult, setSearchwithinResult] = useState('')
-//   const [sectionText, setSectionText] = useState();
+  const [searchwithinResult, setSearchwithinResult] = useState('');
+  //   const [sectionText, setSectionText] = useState();
 
   const onFocus = () => {
     setIsFocsed(!isFocsed);
@@ -34,35 +31,33 @@ function SearchWithinText({
   const onChangeText = (chnage) => {
     setSearchedQuery(chnage);
   };
-  const onItemPress = async() => {
-   
-      // console.log("searchData",searchData);
-  
-      const URL = `${SEARCH_WITH_IN_CASE}caseId=${id}&searchText=${searchedQuery}`;
-      console.log("=====================================",URL);
-      const result = await fetch(URL);
-      const {JudgmentText} = await result.json();
-      setSearchwithinResult(
-        JudgmentText?.length > 0 ? JudgmentText : 'Found in 0 places ',
-      );
-    
-  };
-//   const _renderItem = ({item, index}) => {
-//     const {Value} = item;
-//     return (
-//       <TouchRipple
-//         rippleDuration={800}
-//         rippleColor={appColors.blue}
-//         onPress={() => {
-//           setSearchedQuery(Value);
-//           onItemPress && onItemPress(item);
+  const onItemPress = async () => {
+    // console.log("searchData",searchData);
 
-//         }}
-//         style={{padding: scale(5), borderBottomWidth: scale(0.5)}}>
-//         <CustomLabel text={Value} />
-//       </TouchRipple>
-//     );
-//   };
+    const URL = `${SEARCH_WITH_IN_CASE}caseId=${id}&searchText=${searchedQuery}`;
+    console.log('=====================================', URL);
+    const result = await fetch(URL);
+    const {JudgmentText} = await result.json();
+    setSearchwithinResult(
+      JudgmentText?.length > 0 ? JudgmentText : 'Found in 0 places ',
+    );
+  };
+  //   const _renderItem = ({item, index}) => {
+  //     const {Value} = item;
+  //     return (
+  //       <TouchRipple
+  //         rippleDuration={800}
+  //         rippleColor={appColors.blue}
+  //         onPress={() => {
+  //           setSearchedQuery(Value);
+  //           onItemPress && onItemPress(item);
+
+  //         }}
+  //         style={{padding: scale(5), borderBottomWidth: scale(0.5)}}>
+  //         <CustomLabel text={Value} />
+  //       </TouchRipple>
+  //     );
+  //   };
   ///console.log({searchQuery});
   return (
     <View style={{flex: 1, paddingHorizontal: scale(10)}}>
@@ -87,21 +82,24 @@ function SearchWithinText({
           rightIcon={isFocsed ? 'times' : false}
           containerStyle={{width: '90%'}}
           autoFocus
-          placeholder={"Type your text here"}
+          placeholder={'Type your text here'}
           onSubmitEditing={() => {
             onItemPress();
           }}
         />
       </View>
-      <Highlighter
-        highlightStyle={{
-          backgroundColor: appColors.yellow,
-          fontWeight: '700',
-        }}
-        searchWords={["ram"]}
-        textToHighlight={truncateString(removeHtmlTags(searchwithinResult))}
-        style={styles.bodyText}
-      />
+      {searchedQuery && (
+        <Highlighter
+          highlightStyle={{
+            backgroundColor: appColors.yellow,
+            fontWeight: '700',
+          }}
+          // searchWords={['ram']}
+          searchWords={[...searchedQuery?.split(' ')]}
+          textToHighlight={truncateString(removeHtmlTags(searchwithinResult))}
+          style={styles.bodyText}
+        />
+      )}
       {/* <HTML  source={{ html:searchwithinResult }}  />  */}
       {/* <FlatList
         keyExtractor={(item) =>
@@ -116,15 +114,11 @@ function SearchWithinText({
 
 const styles = StyleSheet.create({
   bodyText: {
-    color: appColors.grayDark
+    color: appColors.grayDark,
   },
-})
-const mapStateToProps = (state) => ({
-  
 });
+const mapStateToProps = (state) => ({});
 
-const mapDispatchToProps = {
-  
-};
+const mapDispatchToProps = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchWithinText);
