522c2fbd696e092ff4949d4d016bc7f45662291b
noddy1996 <shammi.rathore66@gmail.com>
2021-08-11 10:36:27 +0530

    working on download file

--- app/services/downloadFile.js
+++ app/services/downloadFile.js
@@ -1,4 +1,5 @@
 import { DOWNLOAD_JUDGEMENT } from "./ApiList";
+import {PermissionsAndroid, Alert} from "react-native";
 
 export async function downloadFile(id,cname,fontValue,caseName){
     console.log("id",id,"case",cname,"fontValue",fontValue,"caseName",caseName);
@@ -9,6 +10,38 @@ export async function downloadFile(id,cname,fontValue,caseName){
 
 
 }
+const downloadFilePDF = () => {
+    try {
+        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE);
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          this.actualDownload();
+        } else {
+          Alert.alert('Permission Denied!', 'You need to give storage permission to download the file');
+        }
+      } catch (err) {
+        console.warn(err);
+      } 
+  }
+const actualDownload = () => {
+    const { dirs } = RNFetchBlob.fs;
+   RNFetchBlob.config({
+     fileCache: true,
+     addAndroidDownloads: {
+     useDownloadManager: true,
+     notification: true,
+     mediaScannable: true,
+     title: `test.pdf`,
+     path: `${dirs.DownloadDir}/test.pdf`,
+     },
+   })
+     .fetch('GET', 'http://www.africau.edu/images/default/sample.pdf', {})
+     .then((res) => {
+       console.log('The file saved to ', res.path());
+     })
+     .catch((e) => {
+       console.log(e)
+     });
+ }
 
 // https://www.legitquest.com/api/v1.0/download-judgment?id=1ABB&cname=supreme%20court%20of%20india&fontvalue=4&caseName=7777
 // https://www.legitquest.com/api/v1.0/download-judgment?id=ZjYxNTk=&cname=high%20court%20of%20himachal%20pradesh&fontvalue=4&caseName=F6159
